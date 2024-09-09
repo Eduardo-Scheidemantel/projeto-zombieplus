b4badc3f366c4dbc47b452a91f5e4142e2c1b725
Eduardo-Scheidemantel <eduardo.scheidemantel@gmail.com>
2024-09-09 10:42:43 -0300

    Primeiro commit

---
+++ tests/pages/Components.js
@@ -0,0 +1,16 @@
+import exp from 'constants'
+
+const { expect } = require('@playwright/test')
+
+export class Toast {
+
+    constructor(page) {
+        this.page = page
+    }
+
+    async containText(message) {
+        const toast = this.page.locator('.toast')
+
+        await expect(toast).toContainText(message)
+    }
+}
