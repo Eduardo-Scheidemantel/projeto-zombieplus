1a8d57ce73634da97ec741b8c75671fbcf6b240b
Eduardo-Scheidemantel <eduardo.scheidemantel@gmail.com>
2024-09-12 15:49:45 -0300

    test: Adicionar token automatico e receber token por API

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -5,22 +5,26 @@ const data = require('../support/fixtures/movies.json')
 const { executeSQL } = require('../support/database')
 
 test('deve ser possível cadastrar um novo filme', async ({ page }) => {
-
     const movie = data.create
     await executeSQL(`DELETE FROM public.movies WHERE title = '${movie.title}';`)
 
     await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
     await page.movies.create(movie)
     await page.toast.containText("Cadastro realizado com sucesso")
+    await page.waitForTimeout(500)
 })
 
 test('não deve cadastrar quando o título é duplicado', async ({ page, request }) => {
+    const movie = data.duplicate
 
-    await request.api.setToken()
+    await executeSQL(`DELETE FROM public.movies WHERE title = '${movie.title}';`)
+
+    await request.api.postMovie(movie)
 
     await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
     await page.movies.create(movie)
     await page.toast.containText("Este conteúdo já encontra-se cadastrado no catálogo")
+    await page.waitForTimeout(500)
 })
 
 test('não deve cadastrar sem campos obrigatórios preenchidos', async ({ page }) => {
