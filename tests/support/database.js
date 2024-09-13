49505d7021df109f133f968499ea53dce894b977
Eduardo-Scheidemantel <eduardo.scheidemantel@gmail.com>
2024-09-13 16:56:40 -0300

    chore: Add default resolution and test report methods

--- tests/support/database.js
+++ tests/support/database.js
@@ -17,7 +17,7 @@ export async function executeSQL(sqlScript) {
         const client = await pool.connect()
 
         const result = await client.query(sqlScript)
-        console.log(result.rows)
+        //console.log(result.rows)
 
     } catch (error) {
         console.log('Erro ao tentar executar SQL ' + error)
